817562c4b84c6ed0ab2f1c51e43911398e000038
user34 <user34@user34-PC>
2022-03-28 02:19:30 +0530

    rf

---
+++ done/src/done/Day.java
@@ -0,0 +1,33 @@
+package done;
+
+public class Day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int day =10;
+		switch(day)
+		{
+		case 1:System.out.println("sunday");
+		 break;
+		case 2:System.out.println("mon");
+		 break;
+		case 3:System.out.println("tuesday");
+		 break;
+		case 5:System.out.println("wednesday");
+		 break;
+		case 6:System.out.println("thursday");
+		 break;
+		case 7:System.out.println("friday");
+		 break;
+		case 8:System.out.println("saturday");
+		 break;
+		default:System.out.println("invalid");
+		 
+			
+		
+		
+	}
+	}
+}
+	
