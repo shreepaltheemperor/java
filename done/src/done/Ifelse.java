a13c0efe2620206ffa4e55c663adebbfad666e2f
user34 <user34@user34-PC>
2022-03-28 02:31:33 +0530

    s

---
+++ done/src/done/Ifelse.java
@@ -0,0 +1,23 @@
+package done;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int age=26;
+     if(age>=18)
+     {
+    	 System.out.println("eligible for voting");
+    	 
+     }
+     else 
+     {
+    	 System.out.println("not eligible to vote");
+     }
+    	 
+     
+		
+		
+	}
+
+}
