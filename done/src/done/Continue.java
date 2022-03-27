f6eb0fff0ab31c1c66253b98f9272c60a4ca6f14
user34 <user34@user34-PC>
2022-03-28 02:10:39 +0530

    done

---
+++ done/src/done/Continue.java
@@ -0,0 +1,32 @@
+package done;
+
+import javax.swing.Spring;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int i;
+	     for ( i=1;i<=10;i++)
+	      {
+	    	  if(i==5)
+	    	  {
+	    		 continue;
+	    	  }
+	    	  
+	      }  
+		  System.out.println(i);
+	      }  
+		
+		public static void main(Spring srgs[]) {
+			
+			Continue myframe= new Continue();
+		}
+		
+		
+
+	
+
+	}
+
+
